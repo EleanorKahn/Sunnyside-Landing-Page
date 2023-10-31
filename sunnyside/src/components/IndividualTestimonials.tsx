fbffc657915a16f7db19b35cb005708bf0a3052f
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-10-30 18:27:23 -0700

    putting together

--- sunnyside/src/components/IndividualTestimonials.tsx
+++ sunnyside/src/components/IndividualTestimonials.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const IndividualTestimonials = () => {
     return (
-        <div>IndividualTestimonials</div>
+        <div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h4>Name</h4>
+                <h5>Title</h5>
+            </div>
+        </div>
     );
 };
 
